2e28677e427f2604cb4f040f74183c5520864590
jaya.surya <jaya.surya@hfcl.com>
2024-04-23 18:27:12 +0530

    Angular Completed till tutorial:21

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -6,17 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  
+
   title = 'project';
   showMessage=true; 
   colorName= 'green';
   ColorValue='red';
   bgColorVal='#ccc';
+  
+  //This is one way Data Binding (Component.ts to Component.html )
   clsName='one';
   clsName2='two';
+
+
+  mainClass='main';
+  subClass='sub';
+
+
 SwitchValue='one';
 
 user=[
-  {id:1, name:'Surya'},
+  {id:1, name:'Surya'},   //Array of objects
   {id:2, name:'john'},
 ]
 
@@ -24,13 +34,35 @@ user=[
 
 userId=[{userid:10,username:'cody'},
   {userid:20,username:'cena'},
-  {userid:30,username:'rock'},
+  {userid:30,username:'rock'},       //Array of objects
   {userid:40,username:'roman'},
   {userid:50,username:'randy'},
 ]
+// interpolation step 1
+subTitle='This is the interpolation';
+step=1;
+
+inter = {
+  titleId:'1',              //Objects
+  sub:'Interpolation',    
+  status:'Active'
+};
+
+colorRange='red';
+property='pb';
+placeHolderValue='ENTER THE VALUE';
+
+// This is property binding with that exsiting i have added attribute buinding
+hrefval='https://google.com';
+updated='https://youtube.com';
+
+
 
   readme(){
-    
-    console.log("Clicked")
+    alert("inn")
+  }
+  showAlert(id:any){
+    console.log("inside alert");
+    alert('Alert is success'+ id);
   }
 }
